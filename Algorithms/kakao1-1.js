90a2e8b7585aa2305a4f55e87da142fa08931e91
happyjy <okwoyjy@gmail.com>
2019-09-11 23:42:30 +0900

    2020 KAKAO BLIND RECRUITEMENT ALGORITMS 1번 문제-첫번째 예 return value 로직 추가

--- Algorithms/kakao1-1.js
+++ Algorithms/kakao1-1.js
@@ -45,19 +45,20 @@
 
     //1. 문자열을 비교문자열 길이 단위로 잘라 새로운 배열을 만든다.
     //2. 반복문으로 새로운 배열을 비교해 문자열을 압축해 길이를 보관.
-    //3. 1번을 비교문자열 길이 단위(1,2,3...문자열 길이) 반큼 반복한다.
+    //3. 2번 배열을 비교문자열 길이 단위(1,2,3...문자열 길이) 반큼 반복한다.
     //4. 3번을 반복해 압축한 가장 짧은 길이를 return
 
     //3.
+    const zipStr = [];
     for(var i=0; i<s.length; i++){
       // 비교 문자열 길이
       var cmpChrLength = i+1;
       //1.
       var tempArr = [];
-      debugger;
-      for(var j=0; j<s.length/cmpChrLength; j++){
+      for(var j=0; j<s.length/cmpChrLength; j++){ 
         var start = cmpChrLength*j;
-        var end = cmpChrLength*j + cmpChrLength;
+        var end = cmpChrLength*j + cmpChrLength;1
+        //2.
         tempArr.push(s.slice(start, end));
         //tempArr에 넣으면서 zpingStr을 바로 만들려고 했는데 기능별로 로직을 나누자 ^^
         // if(tempArr.length > 0){
@@ -65,37 +66,64 @@
             //     zipingStr += tempArr[0]
             //   }
             // }
-          }
-          console.log("### tempArr: ", tempArr);
-          
+      }
+      console.log("### tempArr: ", tempArr);
+
+
       var zipingStr = '';
-      for(var k=0; k<tempArr.length; k++){
-        var duplChrNum = 0;
+      var k=0;
+      var l=0;
+
+      //3. 
+      for(k; k<tempArr.length; k++){
+        var duplChrNum = 1;
         var duplChr = '';
         var nextIteratorKIdx = 0;
-        var nextIteratorLIdx = 0;
-
         var continueCompare = true;
-        for(var l=0; l<tempArr.length; l++){
+
+        // debugger;
+        for(l=k+1; l<tempArr.length+1; l++){
           if (continueCompare && tempArr[k] === tempArr[l]) {
             ++duplChrNum;
             duplChr = tempArr[k];
-            nextIteratorKIdx = l+1;
-            nextIteratorLIdx = nextIteratorKIdx+1;
+            nextIteratorKIdx = l;
           } else {
+            if(duplChr === ''){
+              duplChr = tempArr[k];
+              nextIteratorKIdx = k;
+            }
             continueCompare = false;
           }
         }
+        //이부분이 앞축하는데 부가적인 정보를 세팅하는 부분
         k = nextIteratorKIdx;
-        l = nextIteratorLIdx;
-        zipingStr += duplChrNum + duplChr;
+        zipingStr += (duplChrNum === 1 ? '' : duplChrNum) + duplChr;
         continueCompare = true
-        debugger;
+        // debugger;
       }
+
+      //압출한 string 보관 arr에 push
+      zipStr.push(zipingStr);
     }
 
+    //압축한 string 길이확인 -> 제일 짧은 길이 확인 -> 제일 짧은 길이 string 찾기
+    let zipStrLength = zipStr.map(item => {
+      return item.length
+    })
 
-    return answer;
+    zipStrLength.sort((a,b) => a-b);
+
+    let shortestZipStrLenth = zipStrLength[0];
+
+    let shortestZipStr = zipStr.filter(item => {
+      if( item.length === shortestZipStrLenth){
+        return item;
+      }
+    })
+
+    console.log("### shortesZipStr: ", shortestZipStr);
+    return shortestZipStr;
   }
 
-   console.log(solution(arr1));
+  console.log(solution(arr1));
+  console.log(solution(arr2));
