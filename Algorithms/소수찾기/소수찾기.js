53995d5abf263229d2d91d56c32ff10540697a74
happyjy <okwoyjy@gmail.com>
2019-09-20 00:44:51 +0900

    H-index algorithm

--- Algorithms/소수찾기/소수찾기.js
+++ Algorithms/소수찾기/소수찾기.js
@@ -1,6 +1,7 @@
 function solution(numbers) {
   var answer = 0;
 
+  // #소수 찾기
   function isPrime(n){
     const divisor = 2;
     while(n > divisor){
@@ -13,11 +14,14 @@ function solution(numbers) {
     }
   }
 
-  //재귀호출
-  //모든조합다나옴 중복제거 필요
+
+  // #숫자 모든 조합 - 중복제거 필요/ 재귀호출 사용
+  // [고민해보기] 모든 조합 만들기 && 중복 제거 
+  var allAssembleStr = [];
   function makeAllAssemble(str, l, n) {
     if (l === n) {
       console.log("### l === n", str);
+      allAssembleStr.push(str);
     } else {
       for (var i = l; i <= n; i++) {
         str = swap(str, l, i);
@@ -38,6 +42,19 @@ function solution(numbers) {
   debugger;
   var numbersArr = numbers.toString().split('');
   makeAllAssemble(numbersArr, 0, numbersArr.length-1);
+
+  //중복제거 - 숫자 모든 조합중
+  // * allAssembleStr : 이차 배열이라 바로 filter에서 중복 하는 것을 피할 수 없다.
+  //[STUDY] allAssembleStr이 filter안에서도 사용된다.
+  //[STUDY] allAssembleStr.indexOf(v): allAssembleStr 배열 안에서 원하는 배열 인자 값이 몇번째에 있는가?
+  //[STUDY] allAseembleStr.indexOf(v) === i : i는 allSeembleStr의 배열 index value 
+  debugger;
+  // var delDuplStr = allAssembleStr.filter((v,i) => allAssembleStr.indexOf(v) === i)
+  
+  var arrJoined = allAssembleStr.map((v,i) => v.join(""));
+  var delDuplArr = arrJoined.filter((v,i)=> arrJoined.indexOf(v) === i);
+
+  console.log("###delDuplStr", delDuplStr);
 
   return answer;
 }
