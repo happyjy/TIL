e24220bfdf7207a1c46ca5a9d864f5d809d3d1c6
happyjy <okwoyjy@gmail.com>
2019-10-19 11:33:09 +0900

    20191019 팀장님교육 세팅 완료

---
+++ 3. JavaScript/03. DESIGN PATTERN & VIEW PATTERN/readme.md
@@ -0,0 +1,2 @@
+# Notion으로 정리
+https://www.notion.so/happyjy0109/7dc8dc60091f4a9da7bb01c499323dc1
---
+++ 3. JavaScript/99. 팀장님 교육/2019_10_FE/server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const razor = require('node-razor');
+
+//node razor template 추가 
+app.engine('vash', razor({
+  layout: 'layout',
+  // ext: ".cshtml",
+  layoutDir: path.resolve('ECERP/Views/Shared'),
+}));
+app.set('view engine', 'vash');
+app.set('views', path.resolve('ECERP/Views/Shared'));
+
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use(express.static('./'));
+
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
+
+app.listen(2222, function () {
+  console.log('Express server listening on port ' + 8888);
+});
+
+app.get('/ECERP/ECP/sample.html', function (req, resp, next) {
+  console.log(req.query)
+  if (req.query.a == 1) {
+    resp.render('sample', {
+      layout: null,
+      title: "ECOUNT"
+    });
+  } else {
+    resp.render('sample', {
+      layout: 'layout',
+      title: "ECOUNT"
+    });
+  }
+})
