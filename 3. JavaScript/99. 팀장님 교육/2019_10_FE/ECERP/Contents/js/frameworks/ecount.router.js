469dbf4350f757c0c4cc779c608f7a6294972e68
happyjy <okwoyjy@gmail.com>
2019-10-19 14:00:24 +0900

    add eventEentListener hashchane

--- 3. JavaScript/99. 팀장님 교육/2019_10_FE/ECERP/Contents/js/frameworks/ecount.router.js
+++ 3. JavaScript/99. 팀장님 교육/2019_10_FE/ECERP/Contents/js/frameworks/ecount.router.js
@@ -1,9 +1,17 @@
 window.ecount = {}
 ecount.router = (function () {
 
+  window.addEventListener("hashchange", function(){
+    console.log("### hashchageEvent", location.hash);
+
+    let url = getMenu(location.hash.replace("#", "").split("&")[1]);
+    movePage(url);
+  });
+
   return {
-    route: function (menu) {
-      location.hash = `menuType=${menu.menuType}$submenu=${menu.submenu}`;
+    route: function (menuId) {
+      location.hash = `menuId=${menuId}`;
+      // location.hash = `menuType=${menu.menuType}$submenu=${menu.submenu}`;
     }
   }
 })();
